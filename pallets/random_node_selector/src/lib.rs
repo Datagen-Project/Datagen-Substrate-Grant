45de6e495142111736b47d2877fe52510e4e95fd
viac92 <viac92@gmail.com>
2022-08-23 16:45:15 +0200

    Create random number fn

--- pallets/random_node_selector/src/lib.rs
+++ pallets/random_node_selector/src/lib.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use codec::Encode;
+use codec::{Encode, Decode};
+use frame_support::traits::Randomness;
 use scale_info::prelude::vec::Vec;
 pub use pallet::*;
 
@@ -18,15 +19,18 @@ pub mod pallet {
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-		type MyRandomness: Randomness<Self::Hash, Self::BlockNumber>;
+		type Randomness: Randomness<Self::Hash, Self::BlockNumber>;
 	}
 
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
+		/// New random hash.
+		UniqueCreated { hash: T::Hash },
+
 		/// New random number.
-		UniqueCreated { number: T::Hash },
+		UniqueNumber { number: u32 },
 	}
 
 	#[pallet::storage]
@@ -46,20 +50,36 @@ pub mod pallet {
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
+
+		/// Create a new random hash.
 		#[pallet::weight(100)]
 		pub fn crate_random_hash(
-			origin: OriginFor<T>)
-			-> DispatchResult {
+			origin: OriginFor<T>
+		) -> DispatchResult {
 			// Account calling this dispatchable.
 			let _sender = ensure_signed(origin)?;
 				// Random value.
 				let nonce = Self::get_and_increment_nonce();
-				let (random_value, _) = T::MyRandomness::random(&nonce);
+				let (random_value, _) = T::Randomness::random(&nonce);
 			// Write the random value to storage.
 			<RandomNumber<T>>::put(random_value);
-			Self::deposit_event(Event::UniqueCreated{number: random_value});
+			Self::deposit_event(Event::UniqueCreated{hash: random_value});
 
 			Ok(())
+		}
+
+		/// Crate a new random number.
+		#[pallet::weight(100)]
+		pub fn create_random_number(
+			origin: OriginFor<T>
+		) -> DispatchResult {
+			let _sender = ensure_signed(origin)?;
+
+			let random_number = Self::generate_random_number();
+
+			Self::deposit_event(Event::UniqueNumber { number: random_number });
+
+				Ok(())
 		}
 	}
 }
@@ -70,4 +90,13 @@ impl<T: Config> Pallet<T> {
 		Nonce::<T>::put(nonce.wrapping_add(1));
 		nonce.encode()
 	}
+
+	/// Generate a random number
+	fn generate_random_number() -> u32 {
+		let (random_seed, _) = T::Randomness::random(&Self::get_and_increment_nonce());
+		let random_number = <u32>::decode(&mut random_seed.as_ref())
+			.expect("secure hashes should always be bigger than u32; qed");
+		random_number
+	}
 }
+
