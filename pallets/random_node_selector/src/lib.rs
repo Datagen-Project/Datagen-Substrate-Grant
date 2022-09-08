d81f524866c6f59b42220b4de3b1cfd1e3944cb2
viac92 <viac92@gmail.com>
2022-09-08 15:34:58 +0200

    implement first version of random_node_to_check

--- pallets/random_node_selector/src/lib.rs
+++ pallets/random_node_selector/src/lib.rs
@@ -4,6 +4,7 @@ use codec::{Encode, Decode};
 use frame_support::traits::Randomness;
 use scale_info::prelude::vec::Vec;
 pub use pallet::*;
+use sp_core::OpaquePeerId as PeerId;
 
 #[cfg(test)]
 mod mock;
@@ -16,8 +17,8 @@ pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 	use frame_support::traits::Randomness;
-	use sp_core::OpaquePeerId as PeerId;
 	use scale_info::prelude::vec::Vec;
+	use sp_core::OpaquePeerId as PeerId;
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
@@ -34,16 +35,29 @@ pub mod pallet {
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// New random hash.
-		UniqueHash { hash: T::Hash },
 
-		/// New random number.
-		UniqueNumber { number: u32 },
+		/// Last random hash.
+		RandomHash { hash: T::Hash },
 
-		/// Set new node owner.
+		/// Last random number.
+		RandomNumber { number: u32 },
+
+		/// Owner has been set.
 		SetOwner {
 			owner: T::AccountId,
 			peer_id: PeerId,
+		},
+
+		/// Owner has been removed.
+		RemoveOwner {
+			peer_id: PeerId,
+		},
+
+		/// Owner and node to check and random number used to select the owner.
+		OwnerToCheck {
+			owner: T::AccountId,
+			peer_id: PeerId,
+			random_number: u32,
 		},
 
 		/// An array with all node owners.
@@ -52,23 +66,31 @@ pub mod pallet {
 			owners: Vec<(T::AccountId, PeerId)>,
 		},
 
-		/// Remove node owner.
-		RemoveOwner {
-			peer_id: PeerId,
-		},
-
 		/// Number of elements in the map.
 		TotalItemsInMap(u32),
 	}
 
+	/// The las random hash.
+	/// dev - This is only for testing purposes.
 	#[pallet::storage]
 	pub(super) type RandomHash<T: Config> =
 		StorageValue<_, T::Hash>;
 
+
+	/// The last owner to check.
 	#[pallet::storage]
+	pub(super) type OwnerToCheck<T: Config> =
+		StorageValue<_, (T::AccountId, PeerId)>;
+
+
+	/// The last random number.
+	#[pallet::storage]
+	#[pallet::getter(fn random_number)]
 	pub(super) type RandomNumber<T: Config> =
 		StorageValue<_, u32, ValueQuery>;
 
+
+	/// Nonce for generating random number.
 	#[pallet::storage]
 	#[pallet::getter(fn get_nonce)]
 	pub(super) type Nonce<T: Config> = StorageValue<
@@ -78,6 +100,7 @@ pub mod pallet {
 		>;
 
 	/// A map that maintains the ownership of each node.
+	/// [Node Owner, Node PeerId]
 	#[pallet::storage]
 	#[pallet::getter(fn owners)]
 	pub type Owners<T: Config> = CountedStorageMap<
@@ -87,6 +110,7 @@ pub mod pallet {
 		PeerId
 		>;
 
+	// Genesis config for the random node selector pallet.
 	#[pallet::genesis_config]
 	pub struct GenesisConfig<T: Config> {
 		/// The initial node owners.
@@ -116,7 +140,10 @@ pub mod pallet {
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 
+		// Test functions.
+
 		/// Create a new random hash.
+		/// dev - This function is only for testing purposes.
 		#[pallet::weight(100)]
 		pub fn create_random_hash(
 			origin: OriginFor<T>
@@ -128,12 +155,13 @@ pub mod pallet {
 				let (random_value, _) = T::Randomness::random(&nonce);
 			// Write the random value to storage.
 			<RandomHash<T>>::put(random_value);
-			Self::deposit_event(Event::UniqueHash{hash: random_value});
+			Self::deposit_event(Event::RandomHash{hash: random_value});
 
 			Ok(())
 		}
 
 		/// Crate a new random number.
+		/// dev - This function is only for testing purposes.
 		#[pallet::weight(100)]
 		pub fn create_random_number(
 			origin: OriginFor<T>
@@ -143,7 +171,7 @@ pub mod pallet {
 			let random_number = Self::generate_random_number();
 
 			<RandomNumber<T>>::put(random_number);
-			Self::deposit_event(Event::UniqueNumber { number: random_number });
+			Self::deposit_event(Event::RandomNumber { number: random_number });
 
 			Ok(())
 		}
@@ -181,6 +209,7 @@ pub mod pallet {
 		}
 
 		/// Number of elements in the map.
+		/// dev - This function is only for testing purposes.
 		#[pallet::weight(100)]
 		pub fn total_elements(
 			origin: OriginFor<T>
@@ -195,6 +224,9 @@ pub mod pallet {
 			Ok(())
 		}
 
+
+		/// Get the list of all node owners.
+		/// dev - This function is only for development purposes.
 		#[pallet::weight(100)]
 		pub fn get_owners_list(
 			origin: OriginFor<T>
@@ -208,10 +240,54 @@ pub mod pallet {
 			Ok(())
 		}
 
+		/// Generate a random number within a range.
+		/// dev - This function is only for demonstration purposes.
+		#[pallet::weight(100)]
+		pub fn generate_random_number_range(
+			origin: OriginFor<T>,
+			max: u32
+		) -> DispatchResult {
+			let _sender = ensure_signed(origin)?;
+
+			let random_number = Self::generate_random_number_in_range(max);
+
+			Self::deposit_event(Event::RandomNumber { number: random_number });
+
+			Ok(())
+		}
+
+
+		// Production functions.
+
+		/// Select a random owner from the list of owners.
+		/// Emit the selected owner.
+		#[pallet::weight(100)]
+		pub fn random_node_to_check(
+			origin: OriginFor<T>
+		) -> DispatchResult {
+			let _sender = ensure_signed(origin)?;
+
+			let (selected_owner_to_unwrap, random_number) = Self::select_random_node();
+
+			let selected_owner = selected_owner_to_unwrap.unwrap();
+
+			<OwnerToCheck<T>>::put(selected_owner.clone());
+			<RandomNumber<T>>::put(random_number);
+
+			Self::deposit_event(Event::OwnerToCheck {
+				owner: selected_owner.0,
+				peer_id: selected_owner.1,
+				random_number,
+			});
+
+			Ok(())
+		}
+
 	}
 }
 
 impl<T: Config> Pallet<T> {
+	/// Progressive nonce to generate random values.
 	fn get_and_increment_nonce() -> Vec<u8> {
 		let nonce = Nonce::<T>::get();
 		Nonce::<T>::put(nonce.wrapping_add(1));
@@ -224,6 +300,22 @@ impl<T: Config> Pallet<T> {
 		let random_number = <u32>::decode(&mut random_seed.as_ref())
 			.expect("secure hashes should always be bigger than u32; qed");
 		random_number
+	}
+
+	/// Select a random owner and his node_id from the list of owners.
+	/// Return the selected owner and the random number used to select the owner.
+	fn select_random_node() -> (Option<(T::AccountId, PeerId)>, u32) {
+		let owners = <Owners<T>>::iter().collect::<Vec<_>>();
+		let random_number = Self::generate_random_number_in_range(owners.len() as u32);
+		(Some(owners[random_number as usize].clone()), random_number)
+	}
+
+	/// Generate a random number within a range 0 to max.
+	/// This function is used to generate a random number within a range.
+	/// The range is from 0 to max.
+	fn generate_random_number_in_range(max: u32) -> u32 {
+		let random_number = Self::generate_random_number();
+		random_number % max
 	}
 }
 
