1659459f66fad6b6f905d7a019b341edf2a8cbf6
viac92 <viac92@gmail.com>
2022-10-07 11:28:32 +0200

    renaming pallets

--- fast_blockchain/pallets/check-node-computational-work/src/lib.rs
+++ fast_blockchain/pallets/check-node-computational-work/src/lib.rs
@@ -65,7 +65,7 @@ pub mod pallet {
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::weight(100)]
-		pub fn do_something(origin: OriginFor<T>) -> DispatchResult {
+		pub fn check_computational_work(origin: OriginFor<T>) -> DispatchResult {
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
 			// https://docs.substrate.io/main-docs/build/origins/
