5f782c6f428f850e64d50d6353ac4bfceb460501
viac92 <viac92@gmail.com>
2022-10-19 17:33:48 +0200

    add more tests

--- fast_blockchain/pallets/check-node-computational-work/src/tests.rs
+++ fast_blockchain/pallets/check-node-computational-work/src/tests.rs
@@ -1,8 +1,8 @@
 use crate::mock::*;
 use sp_core::sr25519;
-use frame_support::{assert_ok, assert_noop};
+use frame_support::assert_ok;
 
-/// Has to check the correct event when hash_work is called.
+/// Has to check the correct event when hash_work is called, in this case the final event should have true as is_passed value.
 #[test]
 fn correct_hook_event_when_hash_work_is_called() {
 	new_test_ext().execute_with(|| {
@@ -72,6 +72,7 @@ fn correct_hook_event_when_hash_work_is_called() {
 	});
 }
 
+/// Has to check the correct event when hash_work is called, in this case the final event should have false as is_passed value.
 #[test]
 fn correct_hook_event_when_hash_work_is_called_2() {
 	new_test_ext().execute_with(|| {
@@ -140,4 +141,86 @@ fn correct_hook_event_when_hash_work_is_called_2() {
 		}));
 
 	});
+}
+
+/// Has to reset the storage process storage when the final result is emitted.
+/// Has to set set_last_computational_work_is_checked to true.\
+#[test]
+fn correct_reset_when_final_result_is_emitted() {
+	new_test_ext().execute_with(|| {
+
+		run_to_block(8);
+
+		assert_ok!(ComputationalWork::hash_work(Origin::signed(get_account_id_from_seed::<sr25519::Public>("Alice"))));
+		let last_computational_work_is_checked = ComputationalWork::last_computational_work_is_checked();
+		assert!(!last_computational_work_is_checked);
+
+		run_to_block(9);
+		let first_author_has_checked = CheckNodeComputationalWork::first_author_has_checked();
+		assert!(first_author_has_checked);
+
+
+		run_to_block(10);
+		let second_author_has_checked = CheckNodeComputationalWork::second_author_has_checked();
+		assert!(second_author_has_checked);
+
+		run_to_block(11);
+		let third_author_has_checked = CheckNodeComputationalWork::third_author_has_checked();
+		assert!(third_author_has_checked);
+
+		run_to_block(14);
+		let first_author_has_checked = CheckNodeComputationalWork::first_author_has_checked();
+		let second_author_has_checked = CheckNodeComputationalWork::second_author_has_checked();
+		let third_author_has_checked = CheckNodeComputationalWork::third_author_has_checked();
+		assert!(!first_author_has_checked);
+		assert!(!second_author_has_checked);
+		assert!(!third_author_has_checked);
+
+		let first_author = CheckNodeComputationalWork::first_author();
+		let second_author = CheckNodeComputationalWork::second_author();
+		let third_author = CheckNodeComputationalWork::third_author();
+		assert_eq!(first_author, None);
+		assert_eq!(second_author, None);
+		assert_eq!(third_author, None);
+
+		let last_computational_work_is_checked = ComputationalWork::last_computational_work_is_checked();
+		assert!(last_computational_work_is_checked);
+	})
+}
+
+
+/// Has to not change the last computational work if the previous one is not checked.
+#[test]
+fn not_change_last_computational_work_if_not_checked() {
+	new_test_ext().execute_with(|| {
+
+		run_to_block(10);
+		assert_ok!(ComputationalWork::hash_work(Origin::signed(get_account_id_from_seed::<sr25519::Public>("Alice"))));
+
+		let last_computational_work = ComputationalWork::last_computational_work();
+
+		run_to_block(11);
+		assert_ok!(ComputationalWork::hash_work(Origin::signed(get_account_id_from_seed::<sr25519::Public>("Bob"))));
+
+		assert_eq!(last_computational_work, ComputationalWork::last_computational_work());
+	})
+}
+
+///Has to change the last computational work if the previous one is checked.
+#[test]
+fn change_last_computational_work_if_is_checked() {
+	new_test_ext().execute_with(|| {
+
+		run_to_block(8);
+		assert_ok!(ComputationalWork::hash_work(Origin::signed(get_account_id_from_seed::<sr25519::Public>("Alice"))));
+
+		let last_computational_work = ComputationalWork::last_computational_work().unwrap();
+		assert_eq!(last_computational_work.2, get_account_id_from_seed::<sr25519::Public>("Alice"));
+
+		run_to_block(14);
+
+		assert_ok!(ComputationalWork::hash_work(Origin::signed(get_account_id_from_seed::<sr25519::Public>("Alice"))));
+		let last_computational_work = ComputationalWork::last_computational_work().unwrap();
+		assert_eq!(last_computational_work.2, get_account_id_from_seed::<sr25519::Public>("Charlie"));
+	})
 }
