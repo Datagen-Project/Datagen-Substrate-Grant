9a94ce71a0b93df89a811f9958b48c4ba0ef94bc
ysfkel <yusufkel@gmail.com>
2023-09-17 20:03:54 +0400

    updated node and runtime

--- fast_blockchain/node/src/chain_spec.rs
+++ fast_blockchain/node/src/chain_spec.rs
@@ -1,40 +1,71 @@
+// Copyright 2019-2021 Parity Technologies (UK) Ltd.
+// This file is part of Parity Bridges Common.
+
+// Parity Bridges Common is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity Bridges Common is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
+
 use bridge_runtime_common::messages_xcm_extension::XcmBlobHauler;
 use datagen_runtime::{
-	BridgeRialtoMessagesConfig,
-	BridgeRialtoParachainMessagesConfig, BridgeWestendGrandpaConfig,
-	AccountId, AuraConfig,BeefyConfig, BalancesConfig, GenesisConfig, GrandpaConfig, Signature, SudoConfig,
-	SystemConfig, SessionConfig, WASM_BINARY, NodeAuthorizationConfig
+	AccountId, AuraConfig, BalancesConfig, BeefyConfig, BridgeRialtoMessagesConfig,
+	BridgeDatagenParachainMessagesConfig, BridgeWestendGrandpaConfig, GrandpaConfig,
+	RuntimeGenesisConfig, SessionConfig, SessionKeys, Signature, SudoConfig, SystemConfig,
+	WASM_BINARY,NodeAuthorizationConfig
 };
-use sc_service::ChainType;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
+use sp_consensus_beefy::crypto::AuthorityId as BeefyId;
+use sp_consensus_grandpa::AuthorityId as GrandpaId;
 use sp_core::{sr25519, Pair, Public, OpaquePeerId};
-use sc_consensus_grandpa::AuthorityId as GrandpaId;
 use sp_runtime::traits::{IdentifyAccount, Verify};
-use datagen_runtime::opaque::SessionKeys;
 
+/// "Names" of the authorities accounts at local testnet.
+const LOCAL_AUTHORITIES_ACCOUNTS: [&str; 5] = ["Alice", "Bob", "Charlie", "Dave", "Eve"];
+/// "Names" of the authorities accounts at development testnet.
+const DEV_AUTHORITIES_ACCOUNTS: [&str; 1] = [LOCAL_AUTHORITIES_ACCOUNTS[0]];
+/// "Names" of all possible authorities accounts.
+const ALL_AUTHORITIES_ACCOUNTS: [&str; 5] = LOCAL_AUTHORITIES_ACCOUNTS;
+/// "Name" of the `sudo` account.
+const SUDO_ACCOUNT: &str = "Sudo";
 /// "Name" of the account, which owns the with-Westend GRANDPA pallet.
 const WESTEND_GRANDPA_PALLET_OWNER: &str = "Westend.GrandpaOwner";
 /// "Name" of the account, which owns the with-Rialto messages pallet.
 const RIALTO_MESSAGES_PALLET_OWNER: &str = "Rialto.MessagesOwner";
-/// "Name" of the account, which owns the with-RialtoParachain messages pallet.
-const RIALTO_PARACHAIN_MESSAGES_PALLET_OWNER: &str = "RialtoParachain.MessagesOwner";
-
-// The URL for the telemetry server.
-// const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
+/// "Name" of the account, which owns the with-DatagenParachain messages pallet.
+const DATAGEN_PARACHAIN_MESSAGES_PALLET_OWNER: &str = "DatagenParachain.MessagesOwner";
 
 /// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
-pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig>;
+pub type ChainSpec = sc_service::GenericChainSpec<RuntimeGenesisConfig>;
 
-/// Generate a crypto pair from seed.
+/// The chain specification option. This is expected to come in from the CLI and
+/// is little more than one of a number of alternatives which can easily be converted
+/// from a string (`--chain=...`) into a `ChainSpec`.
+#[derive(Clone, Debug)]
+pub enum Alternative {
+	/// Whatever the current runtime is, with just Alice as an auth.
+	Development,
+	/// Whatever the current runtime is, with simple Alice/Bob/Charlie/Dave/Eve auths.
+	LocalTestnet,
+}
+
+/// Helper function to generate a crypto pair from seed
 pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
-	TPublic::Pair::from_string(&format!("//{}", seed), None)
+	TPublic::Pair::from_string(&format!("//{seed}"), None)
 		.expect("static values are valid; qed")
 		.public()
 }
 
 type AccountPublic = <Signature as Verify>::Signer;
 
-/// Generate an account ID from seed.
+/// Helper function to generate an account ID from seed
 pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId
 where
 	AccountPublic: From<<TPublic::Pair as Pair>::Public>,
@@ -42,178 +73,136 @@ where
 	AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
 }
 
-/// Generate an Aura authority key.
-pub fn authority_keys_from_seed(s: &str) -> (AuraId, GrandpaId) {
-	(get_from_seed::<AuraId>(s), get_from_seed::<GrandpaId>(s))
+/// Helper function to generate an authority key for Aura
+pub fn get_authority_keys_from_seed(s: &str) -> (AccountId, AuraId, BeefyId, GrandpaId) {
+	(
+		get_account_id_from_seed::<sr25519::Public>(s),
+		get_from_seed::<AuraId>(s),
+		get_from_seed::<BeefyId>(s),
+		get_from_seed::<GrandpaId>(s),
+	)
 }
 
-pub fn development_config() -> Result<ChainSpec, String> {
-	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
-
-	Ok(ChainSpec::from_genesis(
-		// Name
-		"Development",
-		// ID
-		"dev",
-		ChainType::Development,
-		move || {
-			testnet_genesis(
-				wasm_binary,
-				vec![
-					(
-						get_account_id_from_seed::<sr25519::Public>("Alice"),
-						get_from_seed::<AuraId>("Alice"),
-						get_from_seed::<GrandpaId>("Alice"),
-					),
-					(
-						get_account_id_from_seed::<sr25519::Public>("Bob"),
-						get_from_seed::<AuraId>("Bob"),
-						get_from_seed::<GrandpaId>("Bob"),
-					),
-					(
-						get_account_id_from_seed::<sr25519::Public>("Charlie"),
-						get_from_seed::<AuraId>("Charlie"),
-						get_from_seed::<GrandpaId>("Charlie"),
-					),
-					(
-						get_account_id_from_seed::<sr25519::Public>("Dave"),
-						get_from_seed::<AuraId>("Dave"),
-						get_from_seed::<GrandpaId>("Dave"),
-					),
-				],
-				// Initial PoA authorities
-				vec![authority_keys_from_seed("Alice")],
-				// Sudo account
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
-				// Pre-funded accounts
-				vec![
-					get_account_id_from_seed::<sr25519::Public>("Alice"),
-					get_account_id_from_seed::<sr25519::Public>("Bob"),
-					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-				],
-				true,
-			)
-		},
-		// Bootnodes
-		vec![],
-		// Telemetry
-		None,
-		// Protocol ID
-		None,
-		None,
-		// Properties
-		None,
-		// Extensions
-		None,
-	))
+impl Alternative {
+	/// Get an actual chain config from one of the alternatives.
+	pub(crate) fn load(self) -> ChainSpec {
+		let properties = Some(
+			serde_json::json!({
+				"tokenDecimals": 9,
+				"tokenSymbol": "DGN"
+			})
+			.as_object()
+			.expect("Map given; qed")
+			.clone(),
+		);
+		match self {
+			Alternative::Development => ChainSpec::from_genesis(
+				"Datagen Development",
+				"datagen",
+				sc_service::ChainType::Development,
+				|| {
+					testnet_genesis(
+						DEV_AUTHORITIES_ACCOUNTS
+							.into_iter()
+							.map(get_authority_keys_from_seed)
+							.collect(),
+						get_account_id_from_seed::<sr25519::Public>(SUDO_ACCOUNT),
+						endowed_accounts(),
+						true,
+					)
+				},
+				vec![],
+				None,
+				None,
+				None,
+				properties,
+				None,
+			),
+			Alternative::LocalTestnet => ChainSpec::from_genesis(
+				"Datagen Local",
+				"datagen",
+				sc_service::ChainType::Local,
+				|| {
+					testnet_genesis(
+						LOCAL_AUTHORITIES_ACCOUNTS
+							.into_iter()
+							.map(get_authority_keys_from_seed)
+							.collect(),
+						get_account_id_from_seed::<sr25519::Public>(SUDO_ACCOUNT),
+						endowed_accounts(),
+						true,
+					)
+				},
+				vec![],
+				None,
+				None,
+				None,
+				properties,
+				None,
+			),
+		}
+	}
 }
 
-pub fn local_testnet_config() -> Result<ChainSpec, String> {
-	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
-
-
-
-	Ok(ChainSpec::from_genesis(
-		// Name
-		"Local Testnet",
-		// ID
-		"local_testnet",
-		ChainType::Local,
-		move || {
-			testnet_genesis(
-				wasm_binary,
-					vec![
-					(
-						get_account_id_from_seed::<sr25519::Public>("Alice"),
-						get_from_seed::<AuraId>("Alice"),
-						get_from_seed::<GrandpaId>("Alice"),
-					),
-					(
-						get_account_id_from_seed::<sr25519::Public>("Bob"),
-						get_from_seed::<AuraId>("Bob"),
-						get_from_seed::<GrandpaId>("Bob"),
-					),
-					(
-						get_account_id_from_seed::<sr25519::Public>("Charlie"),
-						get_from_seed::<AuraId>("Charlie"),
-						get_from_seed::<GrandpaId>("Charlie"),
-					),
-					(
-						get_account_id_from_seed::<sr25519::Public>("Dave"),
-						get_from_seed::<AuraId>("Dave"),
-						get_from_seed::<GrandpaId>("Dave"),
-					),
-				],
-				// Initial PoA authorities
-				vec![
-					authority_keys_from_seed("Alice"),
-					authority_keys_from_seed("Bob"),
-					authority_keys_from_seed("Charlie"),
-					authority_keys_from_seed("Dave")
-					],
-				// Sudo account
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
-				// Pre-funded accounts
-				vec![
-					get_account_id_from_seed::<sr25519::Public>("Alice"),
-					get_account_id_from_seed::<sr25519::Public>("Bob"),
-					get_account_id_from_seed::<sr25519::Public>("Charlie"),
-					get_account_id_from_seed::<sr25519::Public>("Dave"),
-					get_account_id_from_seed::<sr25519::Public>("Eve"),
-					get_account_id_from_seed::<sr25519::Public>("Ferdie"),
-					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
-				],
-				true,
-			)
-		},
-		// Bootnodes
-		vec![],
-		// Telemetry
-		None,
-		// Protocol ID
-		None,
-		// Properties
-		None,
-		None,
-		// Extensions
-		None,
-	))
+/// We're using the same set of endowed accounts on all Datagen chains (dev/local) to make
+/// sure that all accounts, required for bridge to be functional (e.g. relayers fund account,
+/// accounts used by relayers in our test deployments, accounts used for demonstration
+/// purposes), are all available on these chains.
+fn endowed_accounts() -> Vec<AccountId> {
+	let all_authorities = ALL_AUTHORITIES_ACCOUNTS.iter().flat_map(|x| {
+		[
+			get_account_id_from_seed::<sr25519::Public>(x),
+			get_account_id_from_seed::<sr25519::Public>(&format!("{x}//stash")),
+		]
+	});
+	vec![
+		// Sudo account
+		get_account_id_from_seed::<sr25519::Public>(SUDO_ACCOUNT),
+		// Regular (unused) accounts
+		get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+		get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+		// Accounts, used by Westend<>Datage bridge
+		get_account_id_from_seed::<sr25519::Public>(WESTEND_GRANDPA_PALLET_OWNER),
+		get_account_id_from_seed::<sr25519::Public>("Westend.HeadersRelay1"),
+		get_account_id_from_seed::<sr25519::Public>("Westend.HeadersRelay2"),
+		get_account_id_from_seed::<sr25519::Public>("Westend.AssetHubWestendHeaders1"),
+		get_account_id_from_seed::<sr25519::Public>("Westend.AssetHubWestendHeaders2"),
+		// Accounts, used by Rialto<>Datagen bridge
+		get_account_id_from_seed::<sr25519::Public>(RIALTO_MESSAGES_PALLET_OWNER),
+		get_account_id_from_seed::<sr25519::Public>("Rialto.HeadersAndMessagesRelay"),
+		get_account_id_from_seed::<sr25519::Public>("Rialto.OutboundMessagesRelay.Lane00000001"),
+		get_account_id_from_seed::<sr25519::Public>("Rialto.InboundMessagesRelay.Lane00000001"),
+		get_account_id_from_seed::<sr25519::Public>("Rialto.MessagesSender"),
+		// Accounts, used by DatagenParachain<>Datagen bridge
+		get_account_id_from_seed::<sr25519::Public>(DATAGEN_PARACHAIN_MESSAGES_PALLET_OWNER),
+		get_account_id_from_seed::<sr25519::Public>("DatagenParachain.HeadersAndMessagesRelay1"),
+		get_account_id_from_seed::<sr25519::Public>("DatagenParachain.HeadersAndMessagesRelay2"),
+		get_account_id_from_seed::<sr25519::Public>("DatagenParachain.RialtoHeadersRelay1"),
+		get_account_id_from_seed::<sr25519::Public>("DatagenParachain.RialtoHeadersRelay2"),
+		get_account_id_from_seed::<sr25519::Public>("DatagenParachain.MessagesSender"),
+	]
+	.into_iter()
+	.chain(all_authorities)
+	.collect()
 }
 
-/// Configure initial storage state for FRAME modules.
+fn session_keys(aura: AuraId, beefy: BeefyId, grandpa: GrandpaId) -> SessionKeys {
+	SessionKeys { aura, beefy, grandpa }
+}
+
 fn testnet_genesis(
-	wasm_binary: &[u8],
-	authorities: Vec<(AccountId, AuraId, GrandpaId)>,
-	initial_authorities: Vec<(AuraId, GrandpaId)>,
+	initial_authorities: Vec<(AccountId, AuraId, BeefyId, GrandpaId)>,
 	root_key: AccountId,
 	endowed_accounts: Vec<AccountId>,
 	_enable_println: bool,
-) -> GenesisConfig {
-	GenesisConfig {
+) -> RuntimeGenesisConfig {
+	RuntimeGenesisConfig {
 		system: SystemConfig {
-			// Add Wasm runtime to storage.
-			code: wasm_binary.to_vec(),
+			code: WASM_BINARY.expect("Datagen development WASM not available").to_vec(),
 			..Default::default()
 		},
-		session: SessionConfig {
-			keys: authorities
-				.iter()
-				.map(|x| (x.0.clone(), x.0.clone(), session_keys(x.1.clone(), x.2.clone())))
-				.collect::<Vec<_>>(),
-		},
 		balances: BalancesConfig {
-			// Configure endowed accounts with initial balance of 1 << 60.
-			balances: endowed_accounts.iter().cloned().map(|k| (k, 1 << 60)).collect(),
-		},
-		beefy: BeefyConfig::default(),
-		aura: AuraConfig {
-			authorities: vec![]
+			balances: endowed_accounts.iter().cloned().map(|k| (k, 1 << 50)).collect(),
 		},
 		node_authorization: NodeAuthorizationConfig {
 			nodes: vec![
@@ -235,19 +224,22 @@ fn testnet_genesis(
 				)
 			]
 		},
-		grandpa: GrandpaConfig {
-			authorities: initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect(),
-			..Default::default()
+		aura: AuraConfig { authorities: Vec::new() },
+		beefy: BeefyConfig::default(),
+		grandpa: GrandpaConfig { authorities: Vec::new(), ..Default::default() },
+		sudo: SudoConfig { key: Some(root_key) },
+		session: SessionConfig {
+			keys: initial_authorities
+				.iter()
+				.map(|x| {
+					(x.0.clone(), x.0.clone(), session_keys(x.1.clone(), x.2.clone(), x.3.clone()))
+				})
+				.collect::<Vec<_>>(),
 		},
-		sudo: SudoConfig {
-			// Assign network admin rights.
-			key: Some(root_key),
-		},
-		transaction_payment: Default::default(),
 		bridge_westend_grandpa: BridgeWestendGrandpaConfig {
 			// for our deployments to avoid multiple same-nonces transactions:
-			// //Alice is already used to initialize Rialto<->Millau bridge
-			// => let's use //Westend.GrandpaOwner to initialize Westend->Millau bridge
+			// //Alice is already used to initialize Rialto<->Datagen bridge
+			// => let's use //Westend.GrandpaOwner to initialize Westend->Datagen bridge
 			owner: Some(get_account_id_from_seed::<sr25519::Public>(WESTEND_GRANDPA_PALLET_OWNER)),
 			..Default::default()
 		},
@@ -256,9 +248,9 @@ fn testnet_genesis(
 			opened_lanes: vec![datagen_runtime::rialto_messages::ToRialtoXcmBlobHauler::xcm_lane()],
 			..Default::default()
 		},
-		bridge_rialto_parachain_messages: BridgeRialtoParachainMessagesConfig {
+		bridge_datagen_parachain_messages: BridgeDatagenParachainMessagesConfig {
 			owner: Some(get_account_id_from_seed::<sr25519::Public>(
-				RIALTO_PARACHAIN_MESSAGES_PALLET_OWNER,
+				DATAGEN_PARACHAIN_MESSAGES_PALLET_OWNER,
 			)),
 			opened_lanes: vec![
 				datagen_runtime::datagen_parachain_messages::ToDatagenParachainXcmBlobHauler::xcm_lane(
@@ -268,9 +260,4 @@ fn testnet_genesis(
 		},
 		xcm_pallet: Default::default(),
 	}
-}
-
-/// Helper for session keys to map aura id
-fn session_keys(aura: AuraId, grandpa: GrandpaId) -> SessionKeys {
-	SessionKeys { aura, grandpa }
 }
--- fast_blockchain/runtime/src/pallets_config/pallet_check_node_computational_work_config.rs
+++
@@ -1,6 +0,0 @@
-use crate::*;
-
-impl pallet_check_node_computational_work::Config for Runtime {
-	type RuntimeEvent = RuntimeEvent;
-	type FindAuthor = pallet_session::FindAccountFromAuthorIndex<Self, Aura>;
-}
